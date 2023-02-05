743da065132ad32c57e322ba4eb1bd087dbad208
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-05 13:37:08 +0200

    adding functional navigation to NavBarTabs.tsx

--- src/components/NavBarTabs.tsx
+++ src/components/NavBarTabs.tsx
@@ -2,12 +2,16 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import { useRouter } from 'next/router';
 
 export default function NavBarTabs() {
-  const [value, setValue] = React.useState('home');
+  const [value, setValue] = React.useState('');
+  const router = useRouter();
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
+    router.push(newValue)
+
   };
 
   return (
@@ -20,7 +24,7 @@ export default function NavBarTabs() {
         aria-label="primary tabs example"
         
       >
-        <Tab value="home" label={<span className='text-xl'>HOME</span>}/>
+        <Tab value="/" label={<span className='text-xl'>HOME</span>}/>
         <Tab value="posts" label={<span className='text-xl'>POSTS</span>} />
         <Tab value="comments" label={<span className='text-xl'>COMMENTS</span>} />
         <Tab value="portfolio" label={<span className='text-xl'>PORTFOLIO</span>} />
