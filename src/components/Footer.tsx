5a981755daef4672c6b9c50ff4f97168662d3b3c
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-05 11:17:58 +0200

    Adding components to the Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { Divider } from "@mui/material"
 
 const Footer = () => {
   return (
-    <div>© 2023 SPWS. ALL RIGHTS RESERVED.</div>
+    <div className="fixed bottom-0 w-full h-24">
+      <Divider sx={{borderBottomWidth: 3}}/>
+      <div className="text-2xl p-8 text-center">© 2023 SPWS. ALL RIGHTS RESERVED.</div>
+      </div>
   )
 }
 
