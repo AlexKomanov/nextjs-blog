c161268f67cb66a7f68c2d01623943db95d5e9b2
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-05 13:36:17 +0200

    refactoring of the NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
+import NavBarTabs from './NavBarTabs'
 
 const NavBar = () => {
   return (
-    <div>
-      <h1><Link href="/">SPWS</Link></h1>
-      <Link href="/">HOME</Link>
-      <Link href="/posts"> POSTS</Link>
-      <Link href="/comments"> COMMENTS</Link>
-      <Link href="/portfolio"> PORTFOLIO</Link>
-      <Link href="/about"> ABOUT</Link>
+    <div className='mb-4 font-bold flex justify-between items-center m-3'>
+      <h1 className='text-4xl'><Link href="/">SPWS</Link></h1>
+      <NavBarTabs />
     </div>
     
   )
