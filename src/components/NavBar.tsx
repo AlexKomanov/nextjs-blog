2012c7eba553eed03879ba228d098cea29c3e98a
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-05 09:55:35 +0200

    Adding content to the NavBar and Footer

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+      <h1><Link href="/">SPWS</Link></h1>
+      <Link href="/">HOME</Link>
+      <Link href="/posts"> POSTS</Link>
+      <Link href="/comments"> COMMENTS</Link>
+      <Link href="/portfolio"> PORTFOLIO</Link>
+      <Link href="/about"> ABOUT</Link>
+    </div>
+    
   )
 }
 
