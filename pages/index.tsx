5aa2e4036d7ea51b5eea488192c0f63221e2e318
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-08 19:40:12 +0200

    Adding image and style to the index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import styles from '@/styles/Home.module.css'
 import { Inter } from '@next/font/google'
-import Head from 'next/head'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
  
-      <div >
-          It is a Home page
+      <div className='flex items-center flex-col justify-center'>
+          <h2 className='text-2xl font-extrabold mt-3'>It is a Home page</h2>
+          <Image src='/images/macbook.jpg' alt='home' width={900} height={600} style={{marginTop: '15px'}}/>
       </div>
     
   )
