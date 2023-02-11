e87a1b82edad3160bf3d7d6f39fb2ca215301606
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-11 18:32:21 +0200

    Adding logic of rendering of the Posts page

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,7 +1,32 @@
+import SimpleAccordion, { ISimpleAccordionItem } from "@/src/components/general-ui-components/SimpleAccordion"
+import IPost from "@/src/types/IPost";
+import { useEffect, useState } from "react"
 
 const Posts = () => {
+
+  const [posts, setPosts] = useState<IPost[]>([]);
+  
+  const getPosts = () => {
+    fetch('/api/posts')
+    .then((response) => response.json())
+    .then((data) => setPosts(data))
+    console.log(posts)
+  }
+
+  useEffect(getPosts, [])
+  
+  const allPosts: ISimpleAccordionItem[] = posts.map(post => {
+    return {
+      summary: post.title,
+      details: post.body
+    }
+  }) 
+
   return (
-    <div>It is a Posts Page</div>
+    <>
+    <h2 className='text-2xl font-extrabold mt-3 text-center mb-5'>Posts</h2>
+    <SimpleAccordion items={allPosts}/>
+    </>
   )
 }
 
