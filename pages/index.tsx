929917f23caf2775286fcaad2a320cde62ff732f
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-11 18:30:23 +0200

    Changing name of the title

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
  
       <div className='flex items-center flex-col justify-center'>
-          <h2 className='text-2xl font-extrabold mt-3'>It is a Home page</h2>
+          <h2 className='text-2xl font-extrabold mt-3'>Home</h2>
           <Image src='/images/macbook.jpg' alt='home' width={900} height={600} style={{marginTop: '15px'}}/>
       </div>
     
