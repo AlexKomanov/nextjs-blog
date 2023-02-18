bbf309d6695fcafa6d48287d06dbc837add039ba
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-18 12:16:12 +0200

    Adding a logic of snackbar to the posts.tsx

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -2,45 +2,55 @@ import SimpleAccordion, { ISimpleAccordionItem } from "@/src/components/general-
 import SnackbarComponent from "@/src/components/general-ui-components/SnackbarComponent";
 import IPost from "@/src/types/IPost";
 import { getServerAbsoluteUrl } from "@/src/utils/server/serverUtils";
+import { AlertColor } from "@mui/material";
 import path from "path";
 import { FC } from "react"
 
 
 export async function getStaticProps() {
   // get posts
-  let posts : IPost[] = [];
-  const url = path.join(getServerAbsoluteUrl(), '/api/posts')
+  let posts: IPost[] = [];
+  const url = path.join(getServerAbsoluteUrl(), '/api/posts');
+  let severity: AlertColor;
+  let message: string = "";
+
   try {
     const response = await fetch(url);
     posts = await response.json();
+    severity = "success";
+    message = "Fetch Succeeded!"
   } catch (error) {
     console.error(error);
-    
+    severity = "error";
+    message = "Fetch Error!"
+
   }
 
   return {
-    props: {posts},
+    props: { posts, severity, message },
   }
 }
 
 interface IProps {
   posts: IPost[];
+  severity: AlertColor;
+  message: string;
 }
 
-const Posts : FC<IProps> = ({posts}) => {
-  
+const Posts: FC<IProps> = ({ posts, severity, message }) => {
+
   const allPosts: ISimpleAccordionItem[] = posts.map(post => {
     return {
       summary: post.title,
       details: post.body
     }
-  }) 
+  })
 
   return (
     <>
-    <h2 className='text-2xl font-extrabold mt-3 text-center mb-5'>Posts</h2>
-    <SnackbarComponent isOpen={false} duration={5000} severity="error" message="Hello there from Next JS!"/>
-    <SimpleAccordion items={allPosts}/>
+      <h2 className='text-2xl font-extrabold mt-3 text-center mb-5'>Posts</h2>
+      {(severity === "error") && <SnackbarComponent isOpen={true} duration={5000} severity={severity} message={message} />}
+      <SimpleAccordion items={allPosts} />
     </>
   )
 }
