2e7cc6d9b4ee927acc98caaf4faed94dabd824d8
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-23 07:03:52 +0200

    Refactoring of files

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,12 +1,11 @@
 import IPost from '@/src/types/IPost';
-import type { NextApiRequest, NextApiResponse } from 'next'
-
+import type {NextApiRequest, NextApiResponse} from 'next'
 
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<IPost[]>
+    req: NextApiRequest,
+    res: NextApiResponse<IPost[]>
 ) {
-  const posts = await fetch('https://jsonplaceholder.typicode.com/users/1/posts').then(res => res.json());
-  res.status(200).json(posts)
+    const posts = await fetch('https://jsonplaceholder.typicode.com/users/1/posts').then(res => res.json());
+    res.status(200).json(posts)
 }
