f11b5ed64e9330422dc810d3f302bee0eecf25f8
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-23 09:41:13 +0200

    Adding logic to the comments/index.tsx

--- pages/comments/index.tsx
+++ pages/comments/index.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, {FC} from 'react'
+import path from "path";
+import {getServerAbsoluteUrl} from "@/src/utils/server/serverUtils";
+import {AlertColor} from "@mui/material";
+import ICommentShort from "@/src/types/ICommentShort";
+import SnackbarComponent from "@/src/components/general-ui-components/SnackbarComponent";
 
-const Comments = () => {
-  return (
-    <div>It is a Comments page</div>
-  )
+interface IProps {
+    commentsShort: ICommentShort[];
+    severity: AlertColor;
+    message: string;
+
+}
+export async function getServerSideProps() {
+
+    let props: IProps = {
+        commentsShort: [],
+        severity: "success",
+        message: ""
+    }
+
+    let {commentsShort, severity, message} = props;
+
+    // get comments
+    const url = path.join(getServerAbsoluteUrl(), '/api/comments');
+
+
+    try {
+        const response = await fetch(url);
+        commentsShort = await response.json();
+        severity = "success";
+        message = "Fetch Succeeded!"
+    } catch (error) {
+        console.error(error);
+        severity = "error";
+        message = "Fetch Error!"
+
+    }
+
+    return {
+        props: {commentsShort, severity, message},
+    }
+}
+
+interface IProps {
+    commentsShort: ICommentShort[];
+    severity: AlertColor;
+    message: string;
+}
+
+const Comments: FC<IProps> = ({commentsShort, severity, message}) => {
+
+    console.log(commentsShort)
+
+    return (
+        <div>
+            <h2 className='text-2xl font-extrabold mt-3 text-center mb-5'>Comments</h2>
+            {(severity !== "success") &&
+                <SnackbarComponent isOpen={true} duration={5000} severity={severity} message={message}/>}
+        </div>
+    )
 }
 
 export default Comments
