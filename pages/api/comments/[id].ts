44bcb2b035a01d11bc1c07d22dc65c1523498348
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-03-06 19:33:52 +0200

    Adding getComment function to the [id].ts

--- pages/api/comments/[id].ts
+++ pages/api/comments/[id].ts
@@ -1,11 +1,19 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
+import IComment from "@/src/types/IComment";
+import comments from 'data/comments.json'
 
-
+const getComment = (id: number): IComment | undefined => {
+    return comments.find((comment) => comment.id == id);
+}
 export default function handleCommentWithId(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
 
     const {id} = req.query;
-    res.status(200).json({id})
+
+    const comment = getComment(Number(id));
+
+    comment ? res.status(200).json(comment)
+        : res.status(404).send(`The id '${id}' was not found!`);
 }
